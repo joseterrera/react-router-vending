10a759bc8ef480c71acdb0736ccaad76fdc7a953
Jose Terrera <joseterrera@yahoo.com>
2021-01-22 15:23:36 -0800

    adding chips component

--- src/Chips.js
+++ src/Chips.js
@@ -13,4 +13,27 @@ function Chips() {
     const y = window.innerHeight * Math.random();
     setBags(prevBags => [...prevBags, {x, y} ]);
   }
+
+  const b = bags.map((bag, i) => (
+    <img
+      key={i}
+      src={chipsImg}
+      className="bag"
+      style={{ top: `${bag.y}px`, left: `${bag.x}px` }}
+      alt="bag of lay's chips"
+    />
+  ));
+  return (
+    <div className="Chips">
+      <Message>
+        <h1>bags eaten: {bags.length}</h1>
+        <button onClick={handleClick}>nom nom nom</button>
+        <h1>
+          <Link to="/">go back</Link>
+        </h1>
+      </Message>
+    </div>
+  );
 }
+
+export default Chips;
